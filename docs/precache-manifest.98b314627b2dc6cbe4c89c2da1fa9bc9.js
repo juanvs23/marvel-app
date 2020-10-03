ed269541f6e6e27a28703ffaf98c1c247486ea32
Juan Carlos Avila <juanvs23@gmail.com>
2020-10-03 17:53:08 -0400

    fix

---
+++ docs/precache-manifest.98b314627b2dc6cbe4c89c2da1fa9bc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f003ea9f4354c4327608fbf63f5fe2b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d5226b9b36bc6ebcd7a6",
+    "url": "./static/js/2.3981fec9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.3981fec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05eadb0aca237621dce1",
+    "url": "./static/js/main.33edbb6c.chunk.js"
+  },
+  {
+    "revision": "acb8601c6ff2f54571d5",
+    "url": "./static/js/runtime-main.9255d2d2.js"
+  },
+  {
+    "revision": "06125b3c4b8491f67b2afb9efc88e245",
+    "url": "./static/media/MarvelLogo.06125b3c.svg"
+  }
+]);
